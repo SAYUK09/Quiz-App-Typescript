33782b76042ac314c2048c7cfae24e399a9cdd63
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-05-16 16:19:28 +0530

    Style : Styled Landing Page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
 import Button from "@material-ui/core/Button";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 import "./Home.css";
@@ -24,15 +25,20 @@ export function Home() {
       <div className="homeParent">
         <div className="homeBody">
           <div className="headText">
-            <h3>Let's test your knowledge</h3>
-            <h2>So you think you can create content !</h2>
+            <h3>Let's test your knowledge.</h3>
+            <h1>
+              Ever thought of <span className="spanText"> Podcasting </span>!
+            </h1>
+            <h4>Here's a Fun Quiz.</h4>
           </div>
 
           <div className="btnContainer">
-            <Button className={classes.button} variant="contained">
-              Take a Quiz
-              <ArrowForwardIosRoundedIcon />
-            </Button>
+            <Link className="routerLink" to="/quiz">
+              <Button className={classes.button} variant="contained">
+                Take a Quiz
+                <ArrowForwardIosRoundedIcon />
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
